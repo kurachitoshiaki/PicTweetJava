46d5156dcc5da906ad1e03a45687e61b3c53a92c
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2020-03-15 22:01:10 +0900

    Create User.java

---
+++ src/main/java/com/example/business/domain/User.java
@@ -0,0 +1,45 @@
+package com.example.business.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    @Transient
+    private String passwordConfirmation;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+    public void setPasswordConfirmation(String passwordConfirmation) {
+        this.passwordConfirmation = passwordConfirmation;
+    }
+}
