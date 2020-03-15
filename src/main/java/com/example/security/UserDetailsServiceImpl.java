f26ce46237a95821cbe0f0e0e7219e457d7d8be4
kurachitoshiaki <54346923+kurachitoshiaki@users.noreply.github.com>
2020-03-15 22:01:05 +0900

    Create UserDetailsServiceImpl.java

---
+++ src/main/java/com/example/security/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.security;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.business.domain.User;
+import com.example.business.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+  @Autowired
+  private UserRepository userRepository;
+
+  @Override
+  @Transactional(readOnly = true)
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+	  User user = userRepository.findByEmail(email);
+    Set<GrantedAuthority> grantedAutorities = new HashSet<>();
+
+    return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), grantedAutorities);
+  }
+}
